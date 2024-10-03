ab998bb755f1840dba57c5177e909b22f08a09fd
Melvin Solis <761512@student.dusd.net>
2024-10-03 15:01:49 -0700

    I created my variables and functions scene
    I made my script and added it to my GameObject.

---
+++ ScriptingProjectP8Solis/Assets/Scripts/VariablesAndFunctions.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    //Create a variable of type integer
+    int myInt = 20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //I am going to run myInt through my function to multiply it by two
+        myInt = MultiplyByTwo(myInt);
+
+        //I'm going to print the results to the console
+        Debug.Log(myInt);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Creating a function that takes a int parameter and multiplies it by 2
+    int MultiplyByTwo(int number)
+    {
+        //creating a variable to store my results
+        int results;
+        //multiplying the passed parameter by 2
+        results = number * 2;
+
+        //Returning the results
+        return results;
+
+    }
+
+}
