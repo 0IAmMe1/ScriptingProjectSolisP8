929627e486962b37052284e68ae9920eff2c8c8e
Melvin Solis <761512@student.dusd.net>
2025-05-27 14:32:05 -0700

    practice for exam

---
+++ ScriptingProjectP8Solis/Assets/Scripts/ProgrammerExamPractice.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgrammerExamPractice : MonoBehaviour
+{
+    AudioSource source;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        source.Play();
+    }
+}
